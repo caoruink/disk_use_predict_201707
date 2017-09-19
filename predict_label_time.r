c2e3a9ba58cd450a05e4d8f9f27bfd775add2a2b
Grace R Cao <458101824@qq.com>
2017-09-19 16:34:57 +0800

    Update predict_label_time.r

--- predict_label_time.r
+++ predict_label_time.r
@@ -2,6 +2,8 @@ setwd("E:\\disk_use_201707")
 library(party)
 library(caret)
 library(forecast)
+library(e1071)  # svm
+library(tseries)
 label_sample <- fuction(samples)
 {
   num_sample <- nrow(samples)
@@ -89,21 +91,26 @@ rand <- function(filename)
     cat("end", folds_index, "th train of", filename, "at", as.character(Sys.time()), "\n")
     # find the original file
     total_test <- length(test_hostname)
+    error <- NULL
     i = 1
     while(i <= total_test)
     {
       if(predict_result[i] == 'B')
       {
-        filenam_overd = test_hostname[i]
-        orignal_file <- read.csv(paste("data\\solved_data\\600\\", filenam_overd, ".csv", sep = ""), header = T, as.is = T)
+        print(i)
+        filename_overd = test_hostname[i]
+        orignal_file <- read.csv(paste("data\\solved_data\\600\\", filename_overd, ".csv", sep = ""), header = T, as.is = T)
         orignal_file <- orignal_file[2, 3 : ncol(orignal_file)]
+        cat("************", filename_overd, " ************\n")
         # ARIMA predict
-        ARIMA(orignal_file, 7*144, 15*144)
-        cat("end", filenam_overd, "at", as.character(Sys.time()), "\n")
+        error_line <- ARIMA_svm(orignal_file, 7*144, 15*144)
+        error <- rbind(error, error_line)
+        cat("###########end", filename_overd, "at", as.character(Sys.time()), "###################\n")
       }
       i = i + 1
     }
-    
+    print(error)
+    View(error)
     cat("abnomal value predict\n")
     
     # calculate error rate
@@ -162,19 +169,22 @@ analysis_result <- function(predicted, test_y)
   return(error_all)
 }
 
-ARIMA <- function(ts, length_in, length_pre)
+ARIMA <- function(ts, length_in, length_pre, hostname)
 {
   ts<-as.numeric(ts)
+  # plot(ts,type='l')
   ts_in <- ts[1 : length_in]
   ts_out <- ts[length_in + 1 : length(ts)]
-  arima_ts <- auto.arima(ts_in, stationary = T, seasonal = T)
-  predict_ts <- predict(arima_ts, n.ahead = length_pre)
-  result_pre <- as.numeric(predict_ts$pred)
+  arima_ts <- auto.arima(ts_in)
+  ts_forecast <- forecast(arima_ts, h = length_pre, level = c(99.5))
+  plot(ts_forecast, ylim=c(70,80))
+  
+  result_pre <- as.numeric(ts_forecast['mean']$mean)
   total_length_ts_out <- length(ts_out)
   i = 1
   flag_o = F
   flag_p = F
-  while(i <= total_length_ts_out)
+  while(i <= min(total_length_ts_out, length_pre))
   {
     if(ts_out[i] >= 75 && flag_o == F)
     {
@@ -183,7 +193,7 @@ ARIMA <- function(ts, length_in, length_pre)
     }
     if(result_pre[i] >= 75 && flag_p == F)
     {
-      cat("original overd is", i, "\n")
+      cat("prediction overd is", i, "\n")
       flag_p = T
     }
     if(flag_p == T && flag_o == T)
@@ -192,6 +202,85 @@ ARIMA <- function(ts, length_in, length_pre)
     }
     i = i + 1
   }
+}
+
+calculate_error_rate <- function(pre, row)
+{
+  return(mean(abs(pre - row)))
+}
+
+ARIMA_svm <- function(ts, length_in, length_pre)
+{
+  # 前五天预测，后两天做残差预测，预测之后十四天的使用度
+  ts<-as.numeric(ts)
+  # plot(ts,type='l')
+  ts_in <- ts[1 : (5 * 144)]
+  ts_svm_test <- ts[(5 * 144 + 1) : length_in]
+  ts_out <- ts[(length_in + 1) : length(ts)]
+  # 先预测两天的做残差建模
+  arima_ts <- auto.arima(ts_in)
+  ts_forecast <- forecast(arima_ts, h = 2 * 144, level = c(99.5))
+
+  result_pre <- as.numeric(ts_forecast['mean']$mean)
+  residuals_pre_ori <- result_pre - ts_svm_test
+  residuals_pre_ori <- as.matrix(residuals_pre_ori)
+  newmatrix <- matrix(NA, 4, length(residuals_pre_ori) - 6)
+  for(i in 1 : ncol(newmatrix))
+  {
+    newmatrix[, i] = c(residuals_pre_ori[i], residuals_pre_ori[i + 1], residuals_pre_ori[i] + 2, residuals_pre_ori[i + 3])
+  }
+  inputdata<-cbind(t(newmatrix[, 1 : ncol(newmatrix)]), residuals_pre_ori[5 : length(residuals_pre_ori) - 3])
+  svmfit <- svm (V5~., data = inputData, type="eps-regression", kernel = "radial", cost = 1000, gamma=0.0001, scale = FALSE)
+
+  # 新的一轮预测，预测十四天
+  ts_in <- c(ts_in, ts_svm_test)
+  arima_ts <- auto.arima(ts_in)
+  ts_forecast <- forecast(arima_ts, h = length_pre, level = c(99.5))
+
+  result_pre <- as.numeric(ts_forecast['mean']$mean)  
+  total_length_ts_out <- length(ts_out)  
+  
+  # 预测残差
+  res_index = 1
+  pre_res_new = NULL
+  while(res_index <= length_pre)
+  {
+    test_svm = residuals_pre_ori[(length(residuals_pre_ori) - 3) : length(residuals_pre_ori)]
+    test_svm = as.matrix(test_svm)
+    row.names(test_svm) <- c("V1", "V2", "V3", "V4")
+    new_res = predict(svmfit, t(test_svm))
+    pre_res_new = c(pre_res_new, new_res)
+    residuals_pre_ori = c(residuals_pre_ori, new_res)
+    res_index = res_index + 1
+  }
+  
+  result_pre <- as.numeric(result_pre) + pre_res_new
+  
+  error1 <- calculate_error_rate(result_pre[1 : min(total_length_ts_out, length_pre)], ts_out[1 : min(total_length_ts_out, length_pre)])
+  error2 <- calculate_error_rate(as.numeric(ts_forecast['mean']$mean)[1 : min(total_length_ts_out, length_pre)], ts_out[1 : min(total_length_ts_out, length_pre)])
+
+  # i = 1
+  # flag_o = F
+  # flag_p = F
+  # while(i <= min(total_length_ts_out, length_pre))
+  # {
+  #   if(ts_out[i] >= 75 && flag_o == F)
+  #   {
+  #     cat("original overd is", i, "\n")
+  #     flag_o = T
+  #   }
+  #   if(result_pre[i] >= 75 && flag_p == F)
+  #   {
+  #     cat("prediction overd is", i, "\n")
+  #     flag_p = T
+  #   }
+  #   if(flag_p == T && flag_o == T)
+  #   {
+  #     return()
+  #   }
+  #   i = i + 1
+  # }
+  return(c(error1, error2))
 }
 
 
